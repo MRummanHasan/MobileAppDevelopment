02418a22b18d76929a64df0cd4a68a57c521f80d
MRummanHasan <mrummanhasan@gmail.com>
2018-07-19 16:08:24 +0500

    storing name, age in db

--- MAD-Database/app/src/main/java/com/example/glab/a10_jul_018/DBHelper.java
+++ MAD-Database/app/src/main/java/com/example/glab/a10_jul_018/DBHelper.java
@@ -2,8 +2,12 @@ package com.example.glab.a10_jul_018;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class DBHelper extends SQLiteOpenHelper{
 
@@ -33,4 +37,58 @@ public class DBHelper extends SQLiteOpenHelper{
         cv.put("Age",ud.age);
         return db.insert("UsersData",null,cv);
     }
+    public UserData getSingleUserData(int id) {
+        SQLiteDatabase db = getReadableDatabase();
+
+        //db.rawQuery("SELECT Name,Age FROM USERSDATA WhERE ID ="+id,null);
+        String idInString = String.valueOf(id);
+
+//        What this cursor do
+        Cursor cursor = db.query("USERDATA", new String[]{"NAME", "AGE"}, "ID=?"
+                , new String[]{idInString}, null, null, null, null);
+
+        if (cursor != null) {
+            cursor.moveToFirst();
+            UserData ud = new UserData(cursor.getString(cursor.getColumnIndex("NAME")),
+            Integer.parseInt(cursor.getString(cursor.getColumnIndex("AGE"))));
+            return ud;
+        } else {
+            return null;
+        }
+    }
+
+    public long Delete_UserData(int id)
+    {
+//        db is initialise every time
+            SQLiteDatabase db = getWritableDatabase();
+//            ambiguous WHERE clause
+            return db.delete("USERDATA","ID=?",new String[]{String.valueOf(id)});
+    }
+    public ArrayList<UserData> getAllUserData(){
+        ArrayList<UserData> udlist = new ArrayList<UserData>();
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM USERSDATA", null);
+
+        if (cursor != null){
+            while (cursor.moveToNext())
+            {
+                udlist.add(new UserData(cursor.getString(cursor.getColumnIndex("NAME")),
+                        Integer.parseInt(cursor.getString(cursor.getColumnIndex("AGE")))));
+            }
+            return udlist;
+        }
+        else {
+            return  null;
+        }
+    }
+
+    public long Update_UserData(String OldName, String newName)
+    {
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put("NAME",newName);
+        return db.update("USERSDATA", cv,"NAME=?", new String[]{OldName});
+    }
+
+
 }
