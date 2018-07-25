33cf04b39198e7fd0434f9920c5628705aece397
MRummanHasan <mrummanhasan@gmail.com>
2018-07-25 22:56:02 +0500

    done with data fetching from api,JSON parsed data

--- AsyncTask/app/src/main/java/com/example/rumman/asynctask/MainActivity.java
+++ AsyncTask/app/src/main/java/com/example/rumman/asynctask/MainActivity.java
@@ -6,6 +6,10 @@ import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+/*
+* Project : Fetching data from API
+* Created by M Rumman Khan 07/25/2018
+*/
 
 public class MainActivity extends AppCompatActivity {
 
--- AsyncTask/app/src/main/java/com/example/rumman/asynctask/fetchData.java
+++ AsyncTask/app/src/main/java/com/example/rumman/asynctask/fetchData.java
@@ -2,6 +2,10 @@ package com.example.rumman.asynctask;
 
 import android.os.AsyncTask;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -9,12 +13,20 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+/*
+ * Project : Fetching data from API
+ * Created by M Rumman Khan 07/25/2018
+ */
+
 public class fetchData extends AsyncTask<Void,Void,Void> {
-String data = "";
+    String data = "";
+    String dataParsed = "";
+    String singleParsed = "";
+
     @Override //background thread
     protected Void doInBackground(Void... voids) {
         try {
-            // api url clocation
+            // api url location
             URL url = new URL("https://www.metaweather.com/api/location/search/?query=london");
             HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
             InputStream inputStream = httpURLConnection.getInputStream();
@@ -22,12 +34,26 @@ String data = "";
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
 
             String line = "";
-            while (line != null){
+            while (line != null) {
                 line = bufferedReader.readLine();
                 data = data + line;
             }
 
+            JSONArray JA = new JSONArray(data);
+            for (int i = 0; i < JA.length(); i++) {
+                JSONObject JO = (JSONObject) JA.get(i);
+                singleParsed = "Title: " + JO.get("title") + "\n"+
+                        "Location Type: " + JO.get("location_type") + "\n"+
+                        "Where On Earth IDentifier: " + JO.get("woeid") + "\n"+
+                        "Latitude, Longitude: " + JO.get("latt_long") + "\n";
+//                        "Distance: " + JO.get("distance") + "\n"
+
+                dataParsed = dataParsed + singleParsed;
+            }
+
         } catch (java.io.IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
             e.printStackTrace();
         }
         return null;
@@ -37,6 +63,6 @@ String data = "";
     protected void onPostExecute(Void aVoid) {
         super.onPostExecute(aVoid);
 
-        MainActivity.fdata.setText(this.data);
+        MainActivity.fdata.setText(this.dataParsed);
     }
 }
